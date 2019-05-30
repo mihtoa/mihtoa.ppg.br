2fa670cf3e604c67906b92ecd9153c0f2b1befb4
Milene Toazza <mitoe@live.com>
2019-05-30 00:00:53 -0300

    Testing babel plugin gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const sass = require('gulp-sass');
 const minifyCSS = require('gulp-csso');
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
+const webpack = require('webpack-stream');
 
 function css() {
   return src('src/css/*.scss')
@@ -16,8 +17,11 @@ function js() {
   return src('src/js/*.js')
     .pipe(sourcemaps.init())
     .pipe(babel({
+      comments: false,
+      minified: true,
       presets: ['@babel/env']
     }))
+    .pipe(webpack())
     .pipe(concat('app.min.js'))
     .pipe(sourcemaps.write())
     .pipe(dest('dist/js'))
@@ -25,7 +29,8 @@ function js() {
 
 exports.js = js;
 exports.css = css;
-exports.default = parallel(css, js);
+//exports.default = parallel(css, js);
+exports.default = parallel(css);
 
 watch('src/css/*.scss', css);
-watch('src/js/*.js', js);
+//watch('src/js/*.js', js);
